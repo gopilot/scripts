312899820e59fea51289364cca0e943345b0a626
Peter Andringa <peter9221@gmail.com>
2015-01-01 16:27:11 -0500

    CNAME file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
-var jade = require('gulp-jade');
 var when = require('when');
-var jsyaml = require('js-yaml');
-var fs = require('fs');
 
 var concat = require('gulp-concat');
 var minifyJS = require('gulp-uglify');
@@ -19,10 +16,10 @@ var static = require('node-static');
 
 // copy over everything from the static folder (images, etc)
 // NOTE: into the root of the out folder
-// gulp.task('static', function(){
-//     return gulp.src('./static/**')
-//         .pipe(gulp.dest('./out'));
-// });
+gulp.task('static', function(){
+    return gulp.src('./static/**')
+        .pipe(gulp.dest('./out'));
+});
 
 gulp.task('scripts', function(){
     var tasks = [];
@@ -49,26 +46,10 @@ gulp.task('scripts', function(){
     return when.all(tasks);
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('default', ['static', 'scripts']);
 
-gulp.task('watch', function() {
-    runServer();
-    gulp.watch('./static/**', ['static']);
-    gulp.watch('./css/*.styl', ['stylus']);
-    gulp.watch('./scripts/**/*.js', ['scripts'])
-    gulp.watch(['./*.jade', './components/*.jade', './info.yaml'], ['html']);
-});
-
-var file = new static.Server('./out');
-function runServer(port) {
-    require('http').createServer(function (request, response) {
-        request.addListener('end', function () {
-            file.serve(request, response);
-        }).resume();
-    }).listen(port || 8000);
-}
-gulp.task('deploy', ['scripts'], function () {
-    var remote = "https://github.com/gopilot/philly.git";
+gulp.task('deploy', ['static', 'scripts'], function () {
+    var remote = "https://github.com/gopilot/static.git";
 
     return gulp.src("./out/**/*")
         .pipe( deploy( remote ) );
